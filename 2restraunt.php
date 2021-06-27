de5d99c61afaf132454528d02e05c07c7cafd8cc
apshetty123 <85991541+apshetty123@users.noreply.github.com>
2021-06-27 17:52:43 +0530

    Update 2restraunt.php

--- 2restraunt.php
+++ 2restraunt.php
@@ -1,37 +1,49 @@
-<?php
-$bread=readline("Enter nos of bread: ");
-$vada=readline("Enter nos of vada: ");
-$samosa=readline("Enter nos of samosa: ");
-$p=readline("Enter pice of vadapav: ");
-$x=readline("price of samosapav: ");
+    <?php
+$bread=readline("Enter nos of bread: ");// 9
+$vada=readline("Enter nos of vada: ");//20
+$samosa=readline("Enter nos of samosa: ");//3
+$p=readline("Enter pice of vadapav: ");//15
+$x=readline("price of samosapav: ");//10
 $min_bread=1;
-if($bread>2)
+
+if($bread>=2)
 
 {
     
     
-    if ($p<$x){
-    
- $result3=samosapav($bread,$samosa);
-     $bread-=($samosa *2);
-     $samosa=0;
-     if ($bread >$min_bread){
-         $result5=vadapav($bread,$vada);
-     }
-     echo $final=$result3+$result5;
-    }
-    else {
-        $result4=vadapav($bread,$vada);
-        $bread-=($vada *2);
-        $vada=0; 
-        if ($bread >$min_bread){
-            $r=samosapav($bread,$samosa);
-        }
-        echo $final2=$result4+$r;
-    }
-
+    if ($p<=$x){
+    $result1=0;
+    $q=0;
+ $val=samosapav($bread,$samosa);
+$a=$val['price'];
+$b=$val['bread'];
+$c=$val['samosa'];
+$remaining_samosa=$samosa-$c;
+if ($b> $min_bread){
+   $result1=vadapav($b,$vada);
+ $q=$result1['price'];
 }
 
+echo $final=$a+$q;
+
+} 
+else{
+    $r=0;
+    $s=0;
+    $val=vadapav($bread,$vada);
+    $x=$val['price'];
+    $y=$val['bread'];
+    $z=$val['vada'];
+    $remaining_vada=$vada-$z;
+    if($y>$min_bread){
+       $r=samosapav($y,$samosa);
+       $s=$r['price'];
+
+    }
+    echo $final=$x+$s;
+}
+
+} 
 else{
     echo "Insufficient Bread";
 }
@@ -39,18 +51,17 @@ function samosapav($bread,$samosa)
 {
     global $x;
     global $min_bread;
-    
-    $result=($samosa *2);
-    
-    if ($result<$bread){
-        $result2=($samosa * $x);
-        return $result2;
+    $samosapav=($samosa*2);
+    if ($samosapav<=$bread){
+        $val['price']=($samosa * $x);
+        $val['bread']=$bread-$samosapav;
+        $val['samosa']=$samosa;
+        return $val;
          }
-         else{
-             if ($bread>$min_bread){
-                return $x;
-             }
-             
+         elseif($bread>$min_bread){
+           
+           return samosapav($bread, $samosa-1);
+           
          }
        
         
@@ -58,19 +69,23 @@ function samosapav($bread,$samosa)
 }
 
 function vadapav($bread,$vada){
+    
     global $p;
     global $min_bread;
     $required=($vada*2);
-    if ($required<$bread){
-        $price=($vada * $p);
-        return $price;
+    if ($required<=$bread){
+        $val['price']=($vada * $p);
+        $val['bread']=$bread-$required;
+        $val['vada']=$vada;
+        return $val;
          }
-         else {
-            if ($bread>$min_bread){
-               return $p;
-            }
+         elseif($bread>$min_bread){
+           
+           return vadapav($bread, $vada-1);
+           
+         }
 
 }
-}
+
 
 ?>
